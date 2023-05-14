0ed5d86d8ed0fe61c500a11228bf4df4116c6b0b
unknown <marlon_tancredo@hotmail.com>
2023-05-14 17:37:30 +0100

    styling

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
-  margin-right: 5rem;
-  margin-left: 5rem;
+  margin-right: 1rem;
+  margin-left: 1rem;
   margin-top: 3rem;
   display: flex;
   flex-direction: row;
