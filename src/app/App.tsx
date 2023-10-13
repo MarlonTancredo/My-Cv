887c4bcf3f17337c651c9e022c5f84a5733260eb
unknown <marlon_tancredo@hotmail.com>
2023-10-13 16:27:05 +0100

    refactor: refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,18 @@
-// import * as S from "./styles";
+import * as S from "./styles";
 
-// import Aside from "../components/Aside";
-// import Content from "../components/Content";
+import Aside from "../components/Aside";
+import Content from "../components/Content";
 
 function App() {
     return (
-        <>
-            <h1>App</h1>
-        </>
-        // <S.AppContainer>
-        //     <S.AsideDiv>
-        //         <Aside />
-        //     </S.AsideDiv>
-        //     <S.CotentDiv>
-        //         <Content />
-        //     </S.CotentDiv>
-        // </S.AppContainer>
+        <S.AppContainer>
+            <S.AsideDiv>
+                <Aside />
+            </S.AsideDiv>
+            <S.CotentDiv>
+                <Content />
+            </S.CotentDiv>
+        </S.AppContainer>
     );
 }
 
